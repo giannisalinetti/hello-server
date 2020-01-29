7e09490fde5ef8e1c429ec02d90cc2f55841e931
Gianni Salinetti <gsalinet@redhat.com>
2020-01-29 01:10:27 +0100

    Add probes

--- main.go
+++ main.go
@@ -6,14 +6,26 @@ import (
 )
 
 func exampleServer(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Content-Type", "text/html")
 	w.Write([]byte("<html>\n<body>\n"))
 	w.Write([]byte("<p>Dockerfile build strategy example.</p>\n"))
 	w.Write([]byte("</body>\n</html>\n"))
 }
 
+func readinessProbe(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("OK\n"))
+}
+
+func livenessProbe(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("OK\n"))
+}
+
 func main() {
 	http.HandleFunc("/", exampleServer)
+	http.HandleFunc("/ready", readinessProbe)
+	http.HandleFunc("/alive", livenessProbe)
 	log.Println("Starting http server")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
